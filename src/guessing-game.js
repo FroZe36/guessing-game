8446de23b11b96a12957c2f4b568290b4d7ea5ce
Danila <AngelofDest1ny@yandex.ru>
2023-10-19 15:16:19 +0300

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.min = 0;
+      this.max = 0;
+      this.answer = 0;
+    }
 
     setRange(min, max) {
-
+      this.max = max
+      this.min = min
     }
 
     guess() {
-
+      this.answer = Math.round((this.max + this.min) / 2)
+      return this.answer
     }
 
     lower() {
-
+      this.max = this.answer
     }
 
     greater() {
-
+      this.min = this.answer
     }
 }
-
 module.exports = GuessingGame;
